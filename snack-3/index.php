b5bd94ec1b6322b2ec70ed160cf781e740b1518c
Adner16 <giovanni.renda16@gmail.com>
2022-02-25 17:51:40 +0100

    fix

--- snack-3/index.php
+++ snack-3/index.php
@@ -4,7 +4,7 @@ $number_list = [];
 
 do {
    $rand_number = rand(1,10);
-   if (in_array($rand_number, $number_list)){
+   if (!in_array($rand_number, $number_list)){
        $number_list[] = $rand_number;
    }
 } while (count($number_list) <= 15)
