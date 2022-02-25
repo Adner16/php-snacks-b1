ca4f12aa8edcbfa25e69ba4cb2297b2552ad6f9c
Adner16 <giovanni.renda16@gmail.com>
2022-02-25 18:05:45 +0100

    snack-3

--- snack-3/index.php
+++ snack-3/index.php
@@ -3,9 +3,10 @@
 $number_list = [];
 
 do {
-   $rand_number = rand(1,10);
+   $rand_number = rand(1,20);
    if (!in_array($rand_number, $number_list)){
        $number_list[] = $rand_number;
    }
 } while (count($number_list) <= 15);
+var_dump($number_list);
 ?>
